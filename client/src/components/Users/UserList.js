09eb08f7d12be8b171004bec4c9c5d714cd196c8
jOn3th19 <jonethrudio@gmail.com>
2021-08-21 17:30:55 +0800

    created UserList component

---
+++ client/src/components/Users/UserList.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import moment from "moment";
+
+export default class UserList extends Component {
+  state = {
+    users: []
+  };
+
+  componentDidMount = () => {
+  };
+
+  deleteUser = (e, id) => {
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="mt-3">
+          <h3>User List</h3>
+          <table className="table table-striped mt-3">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Birth Date</th>
+                <th>Gender</th>
+                <th>Address</th>
+                <th>Email Address</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                this.state.users ? this.state.users.map(user => {
+                  return (
+                    <tr key={user._id}>
+                      <td>{user.lastName}, {user.firstName} {user.middleName}{user.nameExt === "" ? null : ", " + user.nameExt}</td>
+                      <td>{moment(user.birthDate).format('LL')}</td>
+                      <td>{user.gender}</td>
+                      <td>{user.address}</td>
+                      <td>{user.email}</td>
+                      <td>
+                        <Link to={`/edit/${user._id}`}>
+                          Edit
+                        </Link>
+                        <button type="button" className="mx-3" onClick={(e) => { this.deleteUser(e, user._id); }}>Delete</button>
+                      </td>
+                    </tr>
+                  );
+                }) : null
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
