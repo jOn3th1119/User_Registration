29b6882ed6faeb607d6ac76ae575197c7801814c
jOn3th19 <jonethrudio@gmail.com>
2021-08-21 19:34:15 +0800

    formatting of forms

--- client/src/components/Users/EditUser.js
+++ client/src/components/Users/EditUser.js
@@ -59,49 +59,64 @@ export default class EditUser extends Component {
         <div className='mt-3'>
           <h3>Create New user</h3>
           <form className='mt-3 mb-3'>
-            <div className="form-group mt-3">
-              <label htmlFor="firstName">First Name:</label>
-              <input className="form-control mt-2" autoComplete="off" type="text" name="firstName" id="firstName" onChange={this.onChange} value={this.state.firstName} />
+
+            <div className="form-group row mt-3">
+              <label className="col-sm-2 col-form-label" htmlFor="firstName">First Name:</label>
+              <div className="col-sm-10">
+                <input className="form-control" autoComplete="off" type="text" name="firstName" id="firstName" onChange={this.onChange} value={this.state.firstName} />
+              </div>
             </div>
 
-            <div className="form-group mt-3">
-              <label htmlFor="middleName">Middle Name:</label>
-              <input className="form-control mt-2" autoComplete="off" type="text" name="middleName" id="middleName" onChange={this.onChange} value={this.state.middleName} />
+            <div className="form-group row mt-3">
+              <label className="col-sm-2 col-form-label" htmlFor="middleName">Middle Name:</label>
+              <div className="col-sm-10">
+                <input className="form-control mt-2" autoComplete="off" type="text" name="middleName" id="middleName" onChange={this.onChange} value={this.state.middleName} />
+              </div>
             </div>
 
-            <div className="form-group mt-3">
-              <label htmlFor="lastName">Last Name:</label>
-              <input className="form-control mt-2" autoComplete="off" type="text" name="lastName" id="lastName" onChange={this.onChange} value={this.state.lastName} />
+            <div className="form-group row mt-3">
+              <label className="col-sm-2 col-form-label" htmlFor="lastName">Last Name:</label>
+              <div className="col-sm-10">
+                <input className="form-control mt-2" autoComplete="off" type="text" name="lastName" id="lastName" onChange={this.onChange} value={this.state.lastName} />
+              </div>
             </div>
 
-            <div className="form-group mt-3">
-              <label htmlFor="nameExt">Name Extension:</label>
-              <input className="form-control mt-2" autoComplete="off" type="text" name="nameExt" id="nameExt" onChange={this.onChange} value={this.state.nameExt} />
+            <div className="form-group row mt-3">
+              <label className="col-sm-2 col-form-label" htmlFor="nameExt">Name Extension:</label>
+              <div className="col-sm-10">
+                <input className="form-control mt-2" autoComplete="off" type="text" name="nameExt" id="nameExt" onChange={this.onChange} value={this.state.nameExt} />
+              </div>
             </div>
 
-            <div className="form-group mt-3">
-              <label htmlFor="birthDate">Birth Date:</label>
-              <input className="form-control mt-2" autoComplete="off" type="date" name="birthDate" id="birthDate" onChange={this.onChange} value={this.state.birthDate} />
+            <div className="form-group row mt-3">
+              <label className="col-sm-2 col-form-label" htmlFor="birthDate">Birth Date:</label>
+              <div className="col-sm-10">
+                <input className="form-control mt-2" autoComplete="off" type="date" name="birthDate" id="birthDate" onChange={this.onChange} value={this.state.birthDate} />
+              </div>
             </div>
 
-            <div className="form-group mt-3">
-              <label htmlFor="gender">Gender:</label>
-              <select className="form-control mt-2" name="gender" id="gender" onChange={this.onChange} value={this.state.gender}>
-                <option disabled value=""> -- Select Gender -- </option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-              </select>
+            <div className="form-group row mt-3">
+              <label className="col-sm-2 col-form-label" htmlFor="gender">Gender:</label>
+              <div className="col-sm-10">
+                <select className="form-control mt-2" name="gender" id="gender" onChange={this.onChange} defaultValue={this.state.gender}>
+                  <option disabled value=""> -- Select Gender -- </option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+              </div>
             </div>
 
-            <div className="form-group mt-3">
-              <label htmlFor="address">Address:</label>
-              <input className="form-control mt-2" autoComplete="off" type="text" name="address" id="address" onChange={this.onChange} value={this.state.address} />
+            <div className="form-group row mt-3">
+              <label className="col-sm-2 col-form-label" htmlFor="address">Address:</label>
+              <div className="col-sm-10">
+                <input className="form-control mt-2" autoComplete="off" type="text" name="address" id="address" onChange={this.onChange} value={this.state.address} />
+              </div>
             </div>
 
             {this.state.msg ? <span className='text-danger'>{this.state.msg}</span> : null}
 
-            <div className="form-group mt-3">
-              <button type="submit" className="btn btn-primary" onClick={this.addUser}>Save Todo</button>
+            <div className="form-group mt-5 mx-auto" style={{ marginBottom: "200px", width: "150px" }}>
+              <button type="submit" className="btn btn-primary btn-lg" onClick={this.addUser}>Save User</button>
             </div>
           </form>
         </div>
